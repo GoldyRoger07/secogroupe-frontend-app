b624146fc6d91df1662e8db0c6e7b824accc2b18
GoldyRoger07 <latouche822@example.com>
2026-04-28 16:30:46 -0400

    hfhrwhrwj898787

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Signin } from './pages/signin/signin';
+import { Signup } from './pages/signup/signup';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'signin', component: Signin},
+    {path: 'signup', component: Signup}
+];
