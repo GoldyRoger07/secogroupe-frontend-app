024abff072cd4c7c270cdd2914450a5e74e42d93
GoldyRoger07 <latouche822@gmail.com>
2026-04-29 07:40:04 -0400

    commit signin

--- src/app/pages/signin/signin.ts
+++ src/app/pages/signin/signin.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-signin',
-  imports: [],
+  imports: [FloatLabelModule, InputTextModule, ButtonModule],
   templateUrl: './signin.html',
   styleUrl: './signin.css',
 })
